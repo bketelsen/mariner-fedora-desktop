6f3be37b45c69d139717a63a4139429486bf2f0f
Kyle Gospodnetich <me@kylegospodneti.ch>
2025-04-21 14:09:51 -0700

    Initial commit of build files

---
+++ Containerfile
@@ -0,0 +1,18 @@
+FROM scratch AS ctx
+COPY build_files /
+
+FROM quay.io/fedora-ostree-desktops/silverblue:40
+
+RUN rpm-ostree install dnf5
+
+COPY system_files /
+
+RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
+    --mount=type=tmpfs,dst=/boot \
+    /ctx/build.sh && \
+    ostree container commit
+
+RUN bootc container lint
---
+++ build_files/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Remove fedora-release packages (with nodeps), then immeidately install the mariner replacement
+# This must be a direct RPM URL because we're unable to use the azure linux repos without a valid version in os-release. It'll get updated in the next step.
+rpm -ev $(rpm -qa | grep ^fedora-) --nodeps
+rpm -iv https://packages.microsoft.com/yumrepos/azurelinux-3.0-prod-base-x86_64/Packages/a/azurelinux-release-3.0-25.azl3.noarch.rpm
+
+# Install Azure Linux packages/repositories
+# Note that some cannot be installed due to bzip2 versioning differences breaking bootc at this time.
+dnf5 -y install \
+	azurelinux-repos \
+	azurelinux-repos-amd \
+	azurelinux-repos-extended \
+	azurelinux-repos-ms-non-oss \
+	azurelinux-repos-ms-oss \
+	azurelinux-repos-shared \
+	azurelinux-rpm-macros \
+	azurelinux-sysinfo
+
+# Install Azure Linux kernel
+dnf5 -y remove --no-autoremove \
+	kernel \
+	kernel-core \
+	kernel-modules \
+	kernel-modules-core \
+	kernel-modules-extra
+
+dnf5 -y install \
+	kernel \
+	kernel-drivers-gpu \
+	kernel-drivers-intree-amdgpu \
+	kernel-drivers-sound \
+	kernel-drivers-accessibility \
+	kernel-uvm
+
+# Flatten the kernel, bootc doesn't use /boot
+QUALIFIED_KERNEL="$(dnf5 repoquery --installed --queryformat='%{evr}' "kernel")"
+mv /boot/vmlinuz-"$QUALIFIED_KERNEL" /usr/lib/modules/"$QUALIFIED_KERNEL"/vmlinuz
+
+# Rebuild initramfs
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+
+chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
